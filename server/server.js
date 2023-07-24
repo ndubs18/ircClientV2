f6d9d39601a06ee3b0a6dd0973e92d742e4a54da
Nick Winkler <Nick@Nicks-Air.lan>
2023-07-24 14:03:45 -0700

    starter folder structure and template engine setup

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+//setting up our folder to serve static files like our CSS style
+app.use(express.static(__dirname));
+
+//set the view directory '../client/views
+app.set('views', `${__dirname}/../client/views`);
+app.set('view engine', 'ejs');
+const port = 3000;
+
+
+app.get("/", (req, res) => {
+    res.render('index');
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
