8c5456ecf4056df5e88ac9cc8dec75c06218f4a4
Nick Winkler <nick.winkler1@gmail.com>
2023-07-25 14:10:23 -0700

    added connection to mongo atlas database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const http = require('http');
+//for reading .env environment variables
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 //this will be used to expose the cookie header in the request
 const cookieParser = require('cookie-parser');
-
 // importing the routers (we export these modules in the respective files)
 const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
@@ -14,7 +16,7 @@ const port = process.env.PORT || 3000;
 //setting up our app to serve static files from this path
 app.use(express.static(`${__dirname}/..`));
 
-//set the view directory '../client/views
+//set the view directory ../client/views
 app.set('views', `${__dirname}/../client/views`);
 app.set('view engine', 'ejs');
 
@@ -23,9 +25,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//setting the routes
+//setting the routers to the respective mounting paths
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
+
+/* -------------------------------------------- */
+// connecting to the database
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connection is established successfully! 🎉");
+  });
+/* -------------------------------------------- */
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
