6e85831fe2c7a08d9ebf1524d46a4820b7b41db3
Nick Winkler <nick.winkler1@gmail.com>
2023-08-03 20:24:01 -0700

    created the schema for the user collection and user sign up

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,43 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
+//import bcrypt hashing module
+const { hash } = require("bcrypt");
 
-router.get("/", (req,res) => {
-    res.send("Hello auth.js");
+const User = require('../../models/user.js')
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // 1. check if user already exists
+    console.log('email: ' + email + 'password: ' + password);
+    const user = await User.findOne({ email: email });
+
+    // if user exists already, return error
+    if (user)
+      return res.status(500).json({
+        message: "User already exists! Try logging in. 😄",
+        type: "warning",
+      });
+    // 2. if user doesn't exist, create a new user
+    // hashing the password
+    const passwordHash = await hash(password, 10);
+    const newUser = new User({
+      email: email,
+      password: passwordHash,
+    });
+    // 3. save the user to the database
+    await newUser.save();
+    // 4. send the response
+    res.status(200).json({
+      message: "User created successfully! 🥳",
+      type: "success",
+    });
+  } catch (error) {
+    res.status(500).json({
+      type: "error",
+      message: "Error creating user!",
+      error,
+    });
+  }
   });
   module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 // configuring routes
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   res.render('index');
 });
 module.exports = router;
